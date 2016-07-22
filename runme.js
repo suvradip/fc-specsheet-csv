918caebbbad18b1d1fa945c40d40e6316f80de6d
suvradip saha <suvradip@fusioncharts.com>
2016-07-22 13:16:37 +0530

    now can write individual csv file also

--- runme.js
+++ runme.js
@@ -7,14 +7,32 @@ config.dir='spec-sheets/';
 config.output = 'output/';
 //output filename
 config.outputFilename = 'map-specs.csv';
+//individual spec-sheets folder name
+config.output_specs = config.output+"spec-sheets/";
 
 //store all the file name in the array
 config.files = fs.readdirSync(config.dir);
   
+//csv file header for individual file
+config.csvHeader = "ID,Short Label,Label\n";
 
 //create output folder directory if doesn't exist
-if (!fs.existsSync(config.output))
-  fs.mkdirSync(config.output);
+// if (!fs.existsSync(config.output))
+//   fs.mkdirSync(config.output);
+
+
+var checkDir = function(){
+  var _path = arguments;
+  if(_path.length === 1) {
+    if(!fs.existsSync(_path[0]))
+      fs.mkdirSync(_path[0]);
+  } else {
+    for(var i=0; i<_path.length; i++)
+      checkDir(_path[i]);
+  }  
+};
+
+checkDir(config.output, config.output_specs);
 
 //creating file
 fs.writeFileSync(config.output + config.outputFilename, "Map Name,ID,Short Label,Label\n"); 
@@ -30,8 +48,11 @@ config.files.forEach(function(file){
     preparedData = fileData.match(/---\|---\|---\|---\n([\s\S]*?)\n\n/); 
     preparedData = preparedData[1].trim();
     preparedData = preparedData.split('|').join(',');
-    preparedData = preparedData.split("\n");
+   
+    //writing individual csv files 
+    fs.writeFileSync(config.output + "spec-sheets/"+mapName+".csv", config.csvHeader + preparedData);
 
+    preparedData = preparedData.split("\n");
     for(var i=0; i<preparedData.length; i++)
       preparedData[i] = mapName + "," + preparedData[i];
 
